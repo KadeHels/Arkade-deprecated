669b98c8069c6d40aeee16df937909dad9c060c6
Kade H <kadeiszman@gmail.com>
2021-09-06 19:03:38 -0500

    add resource service

--- arkade/src/app/services/resource.service.ts
+++ arkade/src/app/services/resource.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import data from '../../assets/en.json';
+import data from '../../assets/resources/en.json';
 
 @Injectable({
   providedIn: 'root'
